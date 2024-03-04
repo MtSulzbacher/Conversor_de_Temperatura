f0a1470523f204967f7269bae1547c97424b5980
Mateus Sulzbacher <106339390+MtSulzbacher@users.noreply.github.com>
2024-03-04 05:00:37 -0300

    Update Temperatura.py

--- Temperatura.py
+++ Temperatura.py
@@ -1,8 +1,7 @@
 def celsius_para_fahrenheit(celsius):
     fahrenheit = celsius * 1.8 + 32
     return fahrenheit
-
-
+    
 celsius = float(input("Digite a temperatura em Celsius: "))
 fahrenheit = celsius_para_fahrenheit(celsius)
 print(f"{celsius}°C é igual a {fahrenheit}°F")
